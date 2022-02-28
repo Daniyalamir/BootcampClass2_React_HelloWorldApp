16c7dc7f4e32fc3cdf157f0558768b9a47751fcd
Daniyalamir <udanial555@gmail.com>
2022-03-01 00:37:30 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>react hello world</h2>
+      Hello im a hello world program of react developing by daniyal
+     
+      
+    </div>
+  );
+}
+
+export default App;
